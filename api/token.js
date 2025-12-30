3f0ed263219d4bd53db633cdea3360d759722442
gauravmindaptix26 <gaurav@mindaptix.com>
2025-12-30 14:57:58 +0530

    toekn dfiles add

--- api/token.js
+++ api/token.js
@@ -58,11 +58,11 @@ export default async function handler(req, res) {
 
   res.setHeader("Access-Control-Allow-Origin", origin);
   res.setHeader("Vary", "Origin");
-  res.setHeader("Access-Control-Allow-Methods", "POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   if (req.method === "OPTIONS") return res.status(200).end();
-  if (req.method !== "POST") {
+  if (req.method !== "GET" && req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
